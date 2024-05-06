7f169b922513610e4c5c885950298b60c04b9bd1
matteo granata <matteogranata92@gmail.com>
2024-05-06 19:12:14 +0200

    shows data only if it contains elements and added possibility to add profits or expenses individually

--- client/src/views/PiggyBank.vue
+++ client/src/views/PiggyBank.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="piggyBank-form">
         <h2>PIGGY BANK</h2>
-        <form v-show="userPiggyBanks == 0" @submit.prevent="createPiggyBank">
+        <form @submit.prevent="createProfit">
             <div class="form-group">
                 <label for="nameProfit">name profit:</label>
                 <input type="text" id="piggyBank" v-model="nameProfit" />
@@ -10,6 +10,11 @@
                 <label for="profit">profit:</label>
                 <input type="number" id="piggyBank" v-model="profit" />
             </div>
+            <button type="submit">add profit</button>
+
+        </form>
+        <!-- v-show="userPiggyBanks == 0" -->
+        <form @submit.prevent="createExpense">
             <div class="form-group">
                 <label for="nameExpense">name expense:</label>
                 <input type="text" id="piggyBank" v-model="nameExpense" />
@@ -18,27 +23,42 @@
                 <label for="expense">expense:</label>
                 <input type="number" id="piggyBank" v-model="expense" />
             </div>
-            <button type="submit">add piggyBank</button>
+            <button type="submit">add expense</button>
         </form>
     </div>
     <div class="piggyBank-container">
         <ul v-if="userPiggyBanks.length > 0">
-            <li v-for="piggyBank in userPiggyBanks" :key="piggyBank._id">
-                <p>name profit:</p>
-                <textarea v-model="piggyBank.nameProfit" name="nameProfit" id="nameProfit" cols="10"
-                    rows="1"> {{ piggyBank.nameProfit }}</textarea>
-                <p>profit:</p>
-                <textarea v-model="piggyBank.profit" name="profit" id="profit" cols="10"
-                    rows="1"> {{ piggyBank.profit }}</textarea>
-                <p>name expense:</p>
-                <textarea v-model="piggyBank.nameExpense" name="nameExpense" id="nameExpense" cols="10"
-                    rows="1"> {{ piggyBank.nameExpense }}</textarea>
-                <p>expense:</p>
-                <textarea v-model="piggyBank.expense" name="expense" id="expense" cols="30"
-                    rows="1">{{ piggyBank.expense }}</textarea><br>
-                <button @click="updatePiggyBank(piggyBank._id)">update PiggyBank</button>
-                <button @click="deletePiggyBank(piggyBank._id)">delete PiggyBank</button>
+            <li v-show="piggyBank.nameProfit != null || piggyBank.profit > 0" v-for="piggyBank in userPiggyBanks"
+                :key="piggyBank._id">
+                <div v-show="piggyBank.nameProfit != null">
+                    <p>name profit:</p>
+                    <textarea v-model="piggyBank.nameProfit" name="nameProfit" id="nameProfit" cols="10"
+                        rows="1"> {{ piggyBank.nameProfit }}</textarea>
+                </div>
 
+                <div v-show="piggyBank.profit > 0">
+                    <p>profit:</p>
+                    <textarea v-model="piggyBank.profit" name="profit" id="profit" cols="10"
+                        rows="1"> {{ piggyBank.profit }}</textarea>
+                </div>
+                <button @click="updatePiggyBank(piggyBank._id)">update Profit</button>
+                <button @click="deletePiggyBank(piggyBank._id)">delete Profit</button>
+            </li>
+
+            <li v-show="piggyBank.nameExpense != null || piggyBank.expense > 0" v-for="piggyBank in userPiggyBanks"
+                :key="piggyBank._id">
+                <div v-show="piggyBank.nameExpense != null">
+                    <p>name expense:</p>
+                    <textarea v-model="piggyBank.nameExpense" name="nameExpense" id="nameExpense" cols="10"
+                        rows="1"> {{ piggyBank.nameExpense }}</textarea>
+                </div>
+                <div v-show="piggyBank.expense > 0">
+                    <p>expense:</p>
+                    <textarea v-model="piggyBank.expense" name="expense" id="expense" cols="30"
+                        rows="1">{{ piggyBank.expense }}</textarea>
+                </div>
+                <button @click="updatePiggyBank(piggyBank._id)">update expense</button>
+                <button @click="deletePiggyBank(piggyBank._id)">delete expense</button>
             </li>
 
         </ul>
@@ -78,12 +98,12 @@ export default {
                 console.log('piggy:', response.data)
                 // Update the assignment based on your actual API response structure
                 this.userPiggyBanks = response.data.piggyBanks;
-
+                console.log('piggy somma:', response.data.piggyBanks)
             } catch (error) {
                 console.error(error);
             }
         },
-        async createPiggyBank() {
+        async createProfit() {
             try {
                 const token = localStorage.getItem('token'); // Get token from local storage
                 if (!token) {
@@ -91,7 +111,7 @@ export default {
                     console.error('No token found');
                     return;
                 }
-                const response = await axios.post('/piggybank', { nameProfit: this.nameProfit, profit: this.profit, nameExpense: this.nameExpense, expense: this.expense }, {
+                const response = await axios.post('/piggybank', { nameProfit: this.nameProfit, profit: this.profit }, {
                     headers: { Authorization: `bearer ${token}` },
                 });
                 console.log('piggy bank added:', response.data);
@@ -99,6 +119,25 @@ export default {
                 this.userPiggyBanks.push(response.data.piggyBank)
                 this.nameProfit = ''
                 this.profit = ''
+
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async createExpense() {
+            try {
+                const token = localStorage.getItem('token'); // Get token from local storage
+                if (!token) {
+                    // Handle no token case (e.g., redirect to login)
+                    console.error('No token found');
+                    return;
+                }
+                const response = await axios.post('/piggybank', { nameExpense: this.nameExpense, expense: this.expense }, {
+                    headers: { Authorization: `bearer ${token}` },
+                });
+                console.log('piggy bank added:', response.data);
+
+                this.userPiggyBanks.push(response.data.piggyBank)
                 this.nameExpense = ''
                 this.expense = ''
 
@@ -137,6 +176,7 @@ export default {
                     console.error('No token found');
                     return;
                 }
+
                 const response = await axios.delete(`/piggybank/${piggyBankId}`, {
                     headers: { Authorization: `bearer ${token}` },
 
