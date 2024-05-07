cc94e320cae083afb8c1567f1fdcd979b17792d2
matteo granata <matteogranata92@gmail.com>
2024-05-07 15:32:04 +0200

    created Welcome page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,10 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Register from '../components/Register.vue'
 import Login from '../components/Login.vue'
 import Homepage from '../views/Homepage.vue'
+import Welcome from '../views/Welcome.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: Welcome
+    },
     {
       path: '/homepage',
       name: 'homepage',
---
+++ client/src/views/Welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>Hello wordl!</p>
+    </div>
+</template>
