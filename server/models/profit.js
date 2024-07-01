e722670d089cb6eeaf461da0273a6fbd705e2f56
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:51:43 +0200

    add profit mongoose scheme

---
+++ server/models/profit.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ProfitSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    nameProfit: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+      default: '',
+    },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Profit = mongoose.model("Profit", ProfitSchema);
