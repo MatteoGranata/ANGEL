677d3812d4f7cde23c9cd3cea92c0cba97bc9647
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:52:55 +0200

    update project.js links

--- server/models/project.js
+++ server/models/project.js
@@ -1,19 +1,57 @@
 import mongoose from "mongoose";
+import { User } from "./user.js";
+import { Post } from "./post.js";
+import { Password } from "./password.js";
+import { Timer } from "./timer.js";
+import { Expense } from "./expense.js";
+import { Profit } from "./profit.js";
 
 const projectSchema = new mongoose.Schema(
   {
     author: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "username",
+      ref: User,
       required: true,
     },
     name: {
       type: String,
     },
-    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
-    piggyBanks: [{ type: mongoose.Schema.Types.ObjectId, ref: "piggyBank" }],
-    passwords: [{ type: mongoose.Schema.Types.ObjectId, ref: "password" }],
-    timers: [{ type: mongoose.Schema.Types.ObjectId, ref: "timer" }],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Post,
+      },
+    ],
+    piggyBanks: {
+      expenses: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: Expense,
+        },
+      ],
+      profits: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: Profit,
+        },
+      ],
+      total: {
+        type: Number,
+        default: 0,
+      },
+    },
+    passwords: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Password,
+      },
+    ],
+    timers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Timer,
+      },
+    ],
   },
   { timestamps: true }
 );
