e97715ca2207dab78521c2bdb8e467180eb27b16
matteo granata <matteogranata92@gmail.com>
2024-07-01 10:52:45 +0200

    Added the ability to pin passwords and updated style
    Removed the insertion form, added a single insertion button, and set up automatic information saving.

--- client/src/views/Password.vue
+++ client/src/views/Password.vue
@@ -1,70 +1,58 @@
 <template>
-    <div class="bg-neutral-900 min-h-screen h-fit w-full">
-        <div class="flex flex-col flex-wrap w-full h-full mt-10">
-            <form @submit.prevent="createPassword" class="h-fit flex justify-center">
-                <div class="mt-10 p-5 border-2 border-neutral-600 flex rounded-xl w-80">
-                    <div class="sm:col-span-4">
-                        <label for="password" class="block text-sm font-medium leading-6 text-slate-50">
-                            for what:
-                        </label>
-                        <div class="mt-1 mb-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-neutral-600 focus-within:ring-2 focus-within:ring-inset focus-within:ring-neutral-400 sm:max-w-md">
-                                <input type="text" v-model="content" required
-                                    class="pl-2 outline-none block flex-1 w-60 bg-transparent py-1.5 pl-1 text-slate-50 sm:text-sm sm:leading-6" />
-                            </div>
-                        </div>
-                        <div class="sm:col-span-4">
-                            <label for="password" class="block text-sm font-medium text-slate-50">Password:</label>
-                            <div class="mt-1">
-                                <div
-                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-neutral-600 focus-within:ring-2 focus-within:ring-inset focus-within:ring-neutral-400 sm:max-w-md">
-                                    <input type="password" v-model="secret" required
-                                        class="pl-2 outline-none block flex-1 leading-6 w-60 leanding-6 bg-transparent py-1.5 pl-1 text-slate-50 focus:ring-0 sm:text-sm sm:leading-6" />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mt-2 text-end">
-                            <button type="submit"
-                                class="rounded-md px-1.5 py-0.5 text-sm ring-2 ring-inset ring-neutral-600 hover:bg-neutral-800 text-slate-50">
-                                ADD PASSWORD
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <div class="flex flex-row justify-center h-fit mx-4 mt-10">
-                <ul class="grid grid-cols-1 gap-9 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <div
+    class="min-h-full sm:min-h-[97vh] h-full w-[97vw] sm:w-full pt-3 pr-3 pb-4 pl-3 sm:pl-7 sm:pr-3 bg-ghost mt-5 mb-3 sm:mt-3 sm:mx-3 rounded-xl text-lg">
+    <div class="flex justify-center w-full h-full drop-shadow-3xl">
+        <div class="flex flex-row justify-center w-full h-full">
+                <ul
+                    class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 h-full w-full">
                     <li v-for="password in userPasswords" :key="password._id"
-                        class="flex flex-col border-2 border-neutral-600 rounded-lg w-full h-fit p-2">
+                        class="flex flex-col bg-ghost rounded-xl w-full h-fit p-1 shadow-lg">
                         <div class="resize-none h-fit w-full">
-                            <textarea @keyup="autoUpdate(password._id)" v-model="password.content" name="password"
-                                class="bg-neutral-800 overflow-hidden rounded-md pl-2 pt-0.5 mb-2 resize-none h-full w-full outline-none">
+                            <textarea @keyup="autoUpdate(password._id)" v-model="password.passwordContent"
+                                name="password" placeholder="Name Password..." rows="1"
+                                class="bg-ghost overflow-hidden pl-2 pt-2 mb-2 resize-none h-fit w-full outline-none text-slate-800 font-medium">
                             </textarea>
                         </div>
-                        <div class="flex rounded-md w-full bg-neutral-800">
+                        <div class="flex rounded-md w-full bg-ghost text-slate-800">
                             <input @keyup="autoUpdate(password._id)" v-if="password.show" type="text"
-                                v-model="password.secret"
-                                class="block flex-1 w-60 bg-neutral-800 rounded-lg py-1.5 pl-2 focus:ring-0 sm:text-sm sm:leading-6 outline-none" />
-                            <input @keyup="autoUpdate(password._id)" v-else type="password" v-model="password.secret"
-                                class="block flex-1 w-60 bg-neutral-800 rounded-lg py-1.5 pl-2 focus:ring-0 sm:text-sm sm:leading-6 outline-none" />
+                                v-model="password.secret" name="passwordView"
+                                class="block flex-1 w-60 bg-transparent rounded-xl py-1.5 pl-2 focus:ring-0 sm:leading-6 outline-none" />
+                            <input @keyup="autoUpdate(password._id)" v-else type="password" v-model="password.secret" name="passwordHidden"
+                                class="block flex-1 w-60 bg-transparent rounded-xl py-1.5 pl-2 focus:ring-0 sm:leading-6 outline-none" placeholder="Password..."/>
                             <button @click="togglePasswordVisibility(password)" class="rounded-md px-2.5">
                                 <template v-if="password.show">
                                     <img width="20" height="20"
-                                        src="https://img.icons8.com/ios-glyphs/90/FFFFFF/visible--v1.png"
+                                        src="https://img.icons8.com/ios-glyphs/90/1E293B/visible--v1.png"
                                         alt="visible--v1" />
                                 </template>
                                 <template v-else>
                                     <img width="20" height="20"
-                                        src="https://img.icons8.com/ios-glyphs/90/FFFFFF/invisible.png"
+                                        src="https://img.icons8.com/ios-glyphs/90/1E293B/invisible.png"
                                         alt="invisible" />
                                 </template>
                             </button>
                         </div>
-                        <div class="flex-row flex mt-2">
-                            <button @click="deletePassword(password._id)"
-                                class="rounded-md px-1.5 py-0.5 text-lg text-gray-900 mx-2 shadow-sm ring-2 ring-inset ring-neutral-600 hover:bg-neutral-800">&#128465;</button>
+                        <div class="flex flex-row justify-end sm:justify-between">
+                            <div class="flex-row flex mt-2">
+                                <button @click="deletePassword(password._id)"
+                                class="rounded-md px-2 w-8 h-8 text-lg mx-2 shadow-3xl ring-2 ring-inset ring-slate-100/50 hover:bg-snow">
+                                <img src="../assets/trashcan.png" alt="cancel">
+                            </button>
+                            </div>
+                            <div class="hidden sm:flex mt-2 flex-row flex">
+                                <button @click="pinPassword(password._id)"
+                                    v-bind:class="{ 'bg-smoke ring-2 ring-slate-500/40': password.pin }"
+                                    class="rounded-md px-2 h-8 w-8 text-lg mx-2 shadow-3xl ring-2 ring-inset ring-slate-100/50 hover:bg-neutral-300/40">
+                                    <img src="../assets/pin.png" alt="pin">
+                                </button>
+                            </div>
                         </div>
+                    </li>
+                    <li class="flex h-full w-full sm:w-fit justify-center items-center">
+                        <button @click="createPassword"
+                            class="py-5 m-7 w-20 h-20 flex justify-center rounded-full bg-ghost text-5xl text-slate-600 ring-2 ring-inset ring-snow/70 hover:bg-snow drop-shadow-xl">
+                            +
+                        </button>
                     </li>
                 </ul>
             </div>
@@ -77,7 +65,7 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            content: '',
+            passwordContent: '',
             secret: '',
             userPasswords: [],
         };
@@ -94,10 +82,15 @@ export default {
                     console.error('No token found');
                     return;
                 }
-                const response = await axios.get('/password/', {
+                const projectID = localStorage.getItem("projectID");
+                if (!projectID) {
+                    console.error('No Project found');
+                    return;
+                }
+                const response = await axios.get(`https://pippo-bn7v.onrender.com/project/${projectID}`, {
                     headers: { Authorization: `Bearer ${token}` },
                 });
-                this.userPasswords = response.data.passwords.map(password => ({ ...password, show: false }));
+                this.userPasswords = response.data.passwords.map(passwords => ({ ...passwords, show: false }));
             } catch (error) {
                 console.error(error);
             }
@@ -110,15 +103,15 @@ export default {
                     console.error('No token found');
                     return;
                 }
-                const response = await axios.post('/password', { content: this.content, secret: this.secret }, {
+                const projectID = localStorage.getItem("projectID");
+                if (!projectID) {
+                    console.error('No Project found');
+                    return;
+                }
+                const response = await axios.post('https://pippo-bn7v.onrender.com/password', { passwordContent: this.passwordContent, secret: this.secret, projectId: projectID }, {
                     headers: { Authorization: `bearer ${token}` },
                 });
-                console.log('Password added:', response.data);
-
                 this.userPasswords.push({ ...response.data.password, show: false });
-                this.content = '';
-                this.secret = '';
-
             } catch (error) {
                 console.error(error);
             }
@@ -141,25 +134,46 @@ export default {
                     console.error('Password to update not found');
                     return;
                 }
-                const response = await axios.patch(`/password/${passwordId}`, { content: passwordToUpdate.content, secret: passwordToUpdate.secret }, {
+                const response = await axios.patch(`https://pippo-bn7v.onrender.com/password/${passwordId}`, { passwordContent: passwordToUpdate.passwordContent, secret: passwordToUpdate.secret }, {
                     headers: { Authorization: `bearer ${token}` },
                 });
-
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async pinPassword(passwordId) {
+            try {
+                const token = localStorage.getItem('token');
+                if (!token) {
+                    console.error('No token found');
+                    return;
+                }
+                const passwordToUpdate = this.userPasswords.find(password => password._id == passwordId);
+                if (!passwordToUpdate) {
+                    console.error('Post to update not found');
+                    return;
+                }
+                if (passwordToUpdate.pin === true) {
+                    passwordToUpdate.pin = false;
+                } else if (passwordToUpdate.pin === false) {
+                    passwordToUpdate.pin = true;
+                }
+                const response = await axios.patch(`https://pippo-bn7v.onrender.com/password/${passwordId}`, { pin: passwordToUpdate.pin }, {
+                    headers: { Authorization: `bearer ${token}` },
+                });
             } catch (error) {
                 console.error(error);
             }
         },
         async deletePassword(passwordId) {
             try {
-                const token = localStorage.getItem('token'); // Get token from local storage
+                const token = localStorage.getItem('token'); 
                 if (!token) {
-                    // Handle no token case (e.g., redirect to login)
                     console.error('No token found');
                     return;
                 }
-                const response = await axios.delete(`/password/${passwordId}`, {
+                const response = await axios.delete(`https://pippo-bn7v.onrender.com/password/${passwordId}`, {
                     headers: { Authorization: `bearer ${token}` },
-
                 });
                 console.log('Password removed:', response.data);
                 this.userPasswords = this.userPasswords.filter(password => password._id !== passwordId);
