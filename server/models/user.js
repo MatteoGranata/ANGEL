3d9df42fb9fb905a865ea733edb1aa18fdef589b
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:53:54 +0200

    added email and avatar type

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     username: {
-        type: String,
-        require: true,
-        unique: true
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
     },
     password: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
+    avatar: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true })
-
-export const User = mongoose.model('User', userSchema) 
+export const User = mongoose.model("User", userSchema);
