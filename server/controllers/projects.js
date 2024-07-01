8c6dd28b6ddcd0a6748a827a7124bba6220f446a
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:45:27 +0200

    updated project and fixes error

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -16,12 +16,35 @@ export const createProject = async (req, res) => {
 };
 export const getAllProjects = async (req, res) => {
   const userId = req.user.userId;
-
   try {
-    const projects = await Project.find({ author: userId });
+    const projects = await Project.find({ author: userId })
+      .populate("author")
+      .populate("passwords")
+      .populate("piggyBanks.expenses")
+      .populate("piggyBanks.profits")
+      .populate("posts")
+      .populate("timers");
     res.status(200).json({ projects });
   } catch (error) {
-    restart.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message });
+  }
+};
+export const getProjectById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await Project.findById(id)
+      .populate("author")
+      .populate("passwords")
+      .populate("posts")
+      .populate("piggyBanks.expenses")
+      .populate("piggyBanks.profits")
+      .populate("timers");
+    if (!project) {
+      return res.status(404).send("Project not found");
+    }
+    res.status(200).send(project);
+  } catch (err) {
+    res.status(400).send(err);
   }
 };
 export const updateProject = async (req, res) => {
