2933f25c341a26eb77afa9921c657dca95ffbb51
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:55:43 +0200

    added getprojectbyid routes link

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createProject, deleteProject, getAllProjects, updateProject } from '../controllers/projects.js';
+import { createProject, deleteProject, getAllProjects, getProjectById, updateProject } from '../controllers/projects.js';
 
 const router = express.Router()
 
 router.post('/',createProject );
 router.get('/',getAllProjects );
-// router.get('/',);
+router.get('/:id',getProjectById);
 router.patch('/:id',updateProject );
 router.delete('/:id',deleteProject );
 
