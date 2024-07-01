749ad5aae5fb70f16a46507d81bbd404b98466d0
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:58:23 +0200

    update index.js file for img and project

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import mongoSanitize from "express-mongo-sanitize";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 import usersRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
@@ -14,12 +17,16 @@ import projectRoutes from "./routes/project.js";
 
 import { authenticateToken } from "./middlewares/auth.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(mongoSanitize());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 
 app.use("/users", authenticateToken, usersRoutes);
@@ -34,7 +41,10 @@ mongoose
   .connect(process.env.CONNECTION_URL)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`server running on port: ${PORT}`);
+      console.log(`Server running on port: ${PORT}`);
     });
   })
-  .catch((error) => console.log(error));
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+    process.exit(1); // Exit the process if database connection fails
+  });
