68728556217e6f2ceba1dcc0793e86061d8d409b
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:56:53 +0200

    add upload image

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,18 @@
 import express from 'express'
-import { deleteUserByID, getAllUsers, insertUser, updateUserByID } from '../controllers/users.js'
+
+import { deleteUserByID, getAllUsers, getUserByID, insertImageUser, insertUser, updateUserByID } from '../controllers/users.js'
+import upload from '../middlewares/upload.js'
 
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
-// router.get('/:id', getUserByID)
+router.get('/:id',getUserByID )
 router.post('/', insertUser)
 router.delete('/:id', deleteUserByID)
 router.patch('/:id', updateUserByID)
+router.post('/:id/image',  upload.single('image'),  insertImageUser )
+
+
 
 export default router
