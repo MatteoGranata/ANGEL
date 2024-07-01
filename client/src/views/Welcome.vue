9f61c37b1472288d219db51ebfcbf4daa18ee3fa
matteo granata <matteogranata92@gmail.com>
2024-07-01 17:50:32 +0200

    corrected grammar texts

--- client/src/views/Welcome.vue
+++ client/src/views/Welcome.vue
@@ -7,10 +7,10 @@
         <h1
           class="font-[League-Ghotic] w-full h-fit felx content-center uppercase tracking-wide h-fit sm:text-[8vh] text-5xl"
         >
-          Organizza, Monitora, Raggiungi i Tuoi Obiettivi.
+        Organize, Monitor, Achieve Your Goals.
         </h1>
         <p class="flex w-full sm:w-[45vw] justify-center mt-[3vw] px-2 sm:text-3xl mb-20 text-lg">
-          La Tua Guida per Gestire il Tempo e le Attività.
+          Your Guide to Managing Time and Tasks.
         </p>
       </div>
       <img
@@ -24,7 +24,7 @@
         <h1
           class="font-[League-Ghotic] font-bold w-fit sm:text-6xl lg:text-8xl text-6xl mb-[5vw] text-center uppercase"
         >
-          Come Utilizzare Angel?
+        How to Use Angel?
         </h1>
         <div
           class="w-full h-fit px-2 sm:p-0 sm:w-[80vw] lg:w-[70vw] xl:w-[50vw] flex flex-wrap justify-evenly mb-5"
@@ -100,34 +100,19 @@
             class="flex rounded-lg bg-ghost text-md sm:text-xl text-slate-800 shadow-lg p-4 mx-2 sm:mx-20"
           >
             <p v-if="currentImage === 'timer'" class="m-1">
-              Modalità Calcolatore del Tempo Utilizzate il nostro contatore per tracciare il tempo
-              impiegato in ogni attività. Basta avviare il contatore all'inizio dell'attività e
-              fermarlo quando avete finito. Il tempo totale impiegato verrà automaticamente sommato
-              al progetto. Potete anche stoppare il contatore e riprenderlo il giorno successivo,
-              mantenendo traccia del periodo di tempo totale.
+              Use our timer to track the time spent on each task. Simply start the timer at the beginning of the task and stop it when you are finished. You can also pause the timer and resume it the next day, keeping track of the total time period.
             </p>
             <p v-if="currentImage === 'piggybank'" class="m-1">
-              Gestione delle Finanze Tenete traccia delle spese e delle entrate dei vostri progetti
-              con facilità. Aggiungete ogni transazione per ottenere una panoramica chiara delle
-              finanze del vostro progetto o delle finanze totali.
+              Keep track of your project expenses and income with ease. Add each transaction to get a clear overview of your project's finances or your total finances.
             </p>
             <p v-if="currentImage === 'password'" class="m-1">
-              Salvataggio delle Password Salvate in modo sicuro le vostre password per ogni account
-              o per account creati specificamente per i vostri progetti. Angel mantiene le vostre
-              informazioni al sicuro e facilmente accessibili.
+              Securely save your passwords for each account or for accounts created specifically for your projects. Angel keeps your information safe and easily accessible.
             </p>
             <p v-if="currentImage === 'post'" class="m-1">
-              Note Personali Aggiungete note personali per ricordarvi cosa fare, annotare idee
-              importanti o pianificare le prossime attività. Le note vi aiutano a mantenere tutto
-              ben organizzato e a portata di mano.
+              Add personal notes to remind yourself of tasks to do, jot down important ideas, or plan upcoming activities. Notes help you keep everything well-organized and within reach.
             </p>
             <p v-if="currentImage === 'project'" class="m-1">
-              Quando accedi ad Angel, la prima schermata che vedrai è la homepage. Qui troverai una
-              panoramica dei tuoi progetti, i post e le password che hai pinnato. La homepage è
-              progettata per darti un accesso rapido e semplice alle informazioni più importanti.
-              Puoi creare nuovi progetti o accedere a quelli esistenti. Ogni progetto raggruppa
-              diverse funzionalità che ti aiuteranno a gestire meglio il tuo tempo e le tue
-              attività.
+              When you log in to Angel, the first screen you will see is the homepage. Here you will find an overview of your projects, pinned posts, and passwords. The homepage is designed to give you quick and easy access to the most important information. You can create new projects or access existing ones. Each project groups various features that will help you better manage your time and tasks.
             </p>
           </div>
           <div class="w-full px-1 sm:w-[90vw] xl:w-[60vw] mt-5 drop-shadow-3xl">
@@ -170,11 +155,10 @@
         <h1
           class="font-[League-Ghotic] font-bold w-fit sm:text-8xl text-6xl mb-[2vw] text-center uppercase"
         >
-          Registrati Ora!
+        Sign Up Now!
         </h1>
         <p class="flex text-lg sm:text-xl px-2 w-full">
-          Per iniziare a utilizzare Angel, registrati e crea il tuo primo progetto. Siamo entusiasti
-          di aiutarti a raggiungere i tuoi obiettivi con maggiore efficienza e organizzazione.
+          To start using Angel, sign up and create your first project. We're excited to help you achieve your goals with greater efficiency and organization.
         </p>
         <router-link
           to="/register"
@@ -224,8 +208,7 @@
           </div>
         </div>
         <p class="text-slate-800 text-sm mt-3 sm:mt-6">
-          Angel è stato realizzato solo a scopo didattico come progetto per Start2Impact e non ha
-          fini di lucro. Spero che l'app ti sia utile e ti aiuti a migliorare la tua produttività.
+          Angel has been developed solely for educational purposes as a project for Start2Impact and is non-profit. I hope the app is useful to you and helps improve your productivity.
         </p>
         <hr class="mb-6 border-slate-800 sm:mx-auto" />
         <div class="flex justify-between items-center">
