26023cce4040bf8444f24146621ec66792f257e3
matteo granata <matteogranata92@gmail.com>
2024-07-01 11:56:27 +0200

    add update timer routes

--- server/routes/timer.js
+++ server/routes/timer.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { createTimer, deleteTimer, getTimer, resetTimer, startTimer, stopTimer } from "../controllers/timers.js";
+import { createTimer, deleteTimer, getTimer, resetTimer, startTimer, stopTimer, updateTimer } from "../controllers/timers.js";
 const router = express.Router()
 
 router.post('/', createTimer);
 router.get('/', getTimer);
+router.patch('/:id', updateTimer)
 router.patch('/:id/avvia',startTimer);
 router.patch('/:id/arresta', stopTimer)
 router.put('/:id/reset', resetTimer);
 router.delete('/:id',deleteTimer )
-
 
 export default router;
