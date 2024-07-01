329d046b0ca52312d31be1c16c8bca7eb53b13f0
matteo granata <matteogranata92@gmail.com>
2024-07-01 10:47:44 +0200

    Fixed links and added a link to the projectDetail component.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,25 +2,35 @@ import { createRouter, createWebHistory } from "vue-router";
 import Register from "../components/Register.vue";
 import Login from "../components/Login.vue";
 import Homepage from "../views/Homepage.vue";
+import UserPage from "../views/UserPage.vue";
 import Post from "../views/Post.vue";
 import Welcome from "../views/Welcome.vue";
-import Password from "@/views/Password.vue";
-import PiggyBank from "@/views/PiggyBank.vue";
-import Timer from "@/views/Timer.vue";
-import Project from "@/views/Project.vue";
+import Password from "../views/Password.vue";
+import PiggyBank from "../views/PiggyBank.vue";
+import Timer from "../views/Timer.vue";
+import ProjectDetail from "../views/ProjectView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "welcome",
       component: Welcome,
     },
     {
       path: "/homepage",
       name: "homepage",
       component: Homepage,
+    },
+    {
+      path: "/userpage",
+      name: "userpage",
+      component: UserPage,
+    },
+    {
+      path: "/project",
+      name: "ProjectDetail",
+      component: ProjectDetail,
       children: [
         {
           path: "/post",
@@ -42,14 +52,8 @@ const router = createRouter({
           name: "timer",
           component: Timer,
         },
-        {
-          path: "/project",
-          name: "project",
-          component: Project,
-        },
       ],
     },
-
     {
       path: "/register",
       name: "register",
