9d7d6581ad3b2d3c5691583100e4a0711aa349c3
matteo granata <matteogranata92@gmail.com>
2024-07-01 10:43:06 +0200

    Updated navbar and added style

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,41 +1,46 @@
 <template>
-  <div class="flex justify-center items-center h-screen w-full">
-    <form @submit.prevent="login" class="w-96 h-min ">
-      <div class="space-y-12">
-        <div class="border-2 p-6 border-gray-900/10 pb-12">
-          <h2 class="text-base text-center font-semibold leading-7 text-4xl text-gray-900">LOGIN</h2>
-          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8">
-            <div class="sm:col-span-4">
-              <label for="username"
-                class="block text-sm font-medium text-center text-xl leading-6 text-gray-900">USERNAME:</label>
-              <div class="mt-2">
-                <div
-                  class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input type="text" name="username" id="username" v-model="username"
-                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6" />
-                </div>
-              </div>
+  <section class="bg-snow/40 w-full h-screen backdrop-blur-lg">
+    <div class="flex w-full h-full items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-ghost rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
+        <div class="px-5 pt-4 pb-2 space-y-4 md:space-y-6 drop-shadow-3xl">
+          <router-link to="/" class="flex p-1 w-fit">
+            <img width="25" height="25" src="https://img.icons8.com/ios-filled/100/1E293B/long-arrow-left.png"
+              alt="Back home" />
+          </router-link>
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-slate-800 md:text-2xl">
+            Sign in to your account
+          </h1>
+          <form class="space-y-4 md:space-y-6" @submit.prevent="login">
+            <div>
+              <label for="username" class="block mb-2 font-medium text-slate-800">
+                Your username
+              </label>
+              <input type="username" name="username" id="username" v-model="username"
+                class="bg-ghost border border-slate-800 text-slate-800 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                placeholder="example434" required="true">
             </div>
-            <div class="sm:col-span-4">
-              <label for="password"
-                class="block text-sm font-medium leading-6 text-xl text-center text-gray-900">PASSWORD:</label>
-              <div class="mt-2">
-                <div
-                  class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input type="password" name="password" id="password" v-model="password"
-                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6" />
-                </div>
-              </div>
+            <div>
+              <label for="password" class="block mb-2 font-medium text-slate-800">Password</label>
+              <input type="password" name="password" id="password" v-model="password" placeholder="••••••••"
+                class="bg-ghost border border-slate-800 text-slate-800 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                required="true">
             </div>
-          </div>
-          <div class="mt-2 text-end">
             <button type="submit"
-              class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">LOGIN</button>
-          </div>
+              class="w-full hover:shadow-xl text-slate-800 bg-primary-600 hover:bg-ghost focus:ring focus:ring-slate-800 focus:outline-none font-bold rounded-lg text-lg px-5 py-2.5 text-center">
+              Sign in
+            </button>
+            <p v-if="errorLogin" class="flex items-center bg-red-500 text-slate-800 px-4 py-3 z-20 rounded font-bold">
+              {{ '!! ' + errorLogin + ' !!' }}
+            </p>
+            <p class="text-md font-light text-slate-800">
+              Don’t have an account yet?
+              <router-link to="/register" class="font-bold text-slate-800 hover:underline">Sign up</router-link>
+            </p>
+          </form>
         </div>
       </div>
-    </form>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -46,22 +51,23 @@ export default {
     return {
       username: '',
       password: '',
+      errorLogin: '',
     };
   },
   methods: {
     async login() {
       try {
-        const response = await axios.post("/auth/login", { username: this.username, password: this.password });
+        const response = await axios.post("https://pippo-bn7v.onrender.com/auth/login", { username: this.username, password: this.password });
         console.log('User logged:', response.data);
-        const user = this.username
+        const userId = response.data.user._id
         localStorage.setItem('token', response.data.data);
-        localStorage.setItem('username', user)
+        localStorage.setItem('userId', userId)
         await this.$router.push(`/homepage`)
         window.location.reload();
 
       } catch (error) {
-        console.log(error.response.data.message)
-        return console.log(error)
+        console.error(error)
+        this.errorLogin = error.response.data.message
       }
     }
 
