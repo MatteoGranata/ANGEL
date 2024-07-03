c19f3541f8cf4e70d95bbc2f82a0e9f2c4808785
matteo granata <matteogranata92@gmail.com>
2024-07-03 14:57:54 +0200

    add comment

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,8 @@
 import './assets/style.css'
 
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3000';  // to connect to the local backend
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
